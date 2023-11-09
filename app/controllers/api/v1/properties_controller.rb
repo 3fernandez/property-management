4d366de5fb327b28b8e94e5d1ac9d95c4de94831
Euclides Fernandes <ef.webdev@gmail.com>
2023-11-08 21:10:46 -0300

    Add index action fro properties

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::PropertiesController < ApplicationController
+  def index
+    properties = Property.active
+
+    render json: properties
+  end
 
   def create
     property = Events::Property::Created.create(payload: property_params)
