db69d57e50e3dd0e76fce47ea84c80998d12cf9b
Euclides Fernandes <ef.webdev@gmail.com>
2023-11-08 21:04:29 -0300

    Add filter for deleted properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,6 @@ class Property < ApplicationRecord
   has_many :events, class_name: "Events::Property::BaseEvent"
 
   enum status: { available: 0, sold: 1 }
+
+  scope :active, -> { where(deleted: false) }
 end
